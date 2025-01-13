76a5692944946bc858e060167a608b69e0d17c7f
TvWoerkom <37103738+TvWoerkom@users.noreply.github.com>
2025-01-13 19:36:11 +0100

    Update camera.js

--- camera.js
+++ camera.js
@@ -3,16 +3,16 @@ const video = document.getElementById('camera');
 
 // Check if the browser supports media devices
 if (navigator.mediaDevices && navigator.mediaDevices.getUserMedia) {
-    // Request the camera (video only)
+    // Request access to the camera (video only)
     navigator.mediaDevices.getUserMedia({ video: true })
         .then((stream) => {
             // Assign the camera stream to the video element
             video.srcObject = stream;
         })
         .catch((err) => {
-            console.log('Error accessing the camera: ' + err);
-            alert('Unable to access the camera: ' + err);
+            console.log('Error accessing the camera: ', err);
+            alert('Unable to access the camera: ' + err.message);
         });
 } else {
-    alert('Camera not supported in this browser.');
+    alert('Your browser does not support camera access.');
 }
